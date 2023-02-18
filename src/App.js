eab1ebc861a8496052d7793eb86121edcff07126
bg560581 <bennett.galgano@gmail.com>
2023-02-17 20:13:19 -0500

    rr-2-create-a-react-rainbow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  let [colors, setColors] = useState ([
+    'violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'
+  ]) 
 
-export default App;
+  let colorMap = colors.map((color, i) => {
+    return <ColorBlock key={i} color={color}/>
+  })
+    
+    const checkColor = (newColor) => {
+      const cool = new Option().style;
+      cool.color = newColor
+      return cool.color !== ""
+    }
+  
+    const addColor = (newColor) => {
+      if(checkColor(newColor)){
+      setColors([...colors, newColor])
+    }
+  }
+  
+    return(
+      <div className='App'>
+        <ColorForm addColor={addColor}/>
+        <div className='colorCards'>
+        {colorMap}
+        </div>
+      </div>
+    )
+}
+  
+  export default App
